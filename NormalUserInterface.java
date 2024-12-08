f18223ea609f9f2f2e847c6df8266ab0a2bbbead
brangold123 <brandon.goldstein123@gmail.com>
2024-12-08 17:30:40 -0500

    Add files via upload

---
+++ NormalUserInterface.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//Interface defining the methods that a Normal User should implement to interact with the PhoneBook
+public interface NormalUserInterface {
+	
+	// NormalUser Methods
+    void addPhoneEntry(PhoneBookEntry entry);
+    void editPhoneEntry(String firstName, String lastName, Scanner input);
+    void sortPhoneBookDirectory();
+    void searchLinear(String phoneNumber);
+}
--- PhoneBook.java
+++ PhoneBook.java
@@ -1,23 +1,29 @@
-// Exercise 1
-import java.util.Scanner;
 
 public class PhoneBook {
 	
 	public static void main (String [] args) {
 		
-		PhoneBookEntry [] myInventory = new PhoneBookEntry[3];
-		System.out.println("Welcome to Brandon's, Jackson's, and Vincent's Car Management System!");
+		// Create an array to hold multiple PhoneBookEntry objects
+		PhoneBookEntry [] entries = new PhoneBookEntry[3];
+		
+		// Display a welcome message
+		System.out.println("Welcome to Brandon's, Jackson's, Vincent's, and Henry's PhoneBook Application!");
 		System.out.println();
 		
-		PhoneBookEntry entry1 = new PhoneBookEntry(5,"John","Smith","jsmith@gmail.edu",20037,2023334444);
+		// Create a PhoneBookEntry object for John
+		PhoneBookEntry entry1 = new PhoneBookEntry(5,"John","Smith","jsmith@gmail.edu",20037,"2023334444");
 		
-		PhoneBookEntry entry2 = new PhoneBookEntry("James",202344344);
+		// Create a PhoneBookEntry object for James
+		PhoneBookEntry entry2 = new PhoneBookEntry("James","202344344");
 		
+		// Create a PhoneBookEntry object for Sarah
 		PhoneBookEntry entry3 = new PhoneBookEntry("Sarah");
 		
-		entry1.setPhonenumber(202555555);
+		// Change John's phone number
+		entry1.setPhonenumber("202555555");
 		entry1.printBookEntry();
 		
+		// Change James's zipcode to John's
 		entry2.setZipcode(entry1.getZipcode());
 	}
 }
